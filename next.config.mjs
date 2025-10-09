f858c1ed7386307f4964e514bdf12d611b7c7549
signothecat <shinogoroneko0818@gmail.com>
2025-10-10 03:26:44 +0900

    chore: change next.js devIndicsators position to top-right
    Updated `next.config.mjs`.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: {
+    position: "top-right" // top-right, bottom-right, top-left, bottom-left
+  }
 };
 
 export default nextConfig;
