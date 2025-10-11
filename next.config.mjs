290027afdfaa36f5a7d2608c3fb058e3c34e1374
signothecat <shinogoroneko0818@gmail.com>
2025-10-11 16:00:46 +0900

    chore: hide devIndicators for development
    UI開発で下のコンテンツが見えなくなってしまうので、devIndicatorsを非表示にしました。
    
    Main changes:
    - `next.config.mjs`

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  devIndicators: {
-    position: "top-right" // top-right, bottom-right, top-left, bottom-left
-  }
+  devIndicators:
+    false |
+    {
+      position: "top-right" // top-right, bottom-right, top-left, bottom-left
+    }
 };
 
 export default nextConfig;
