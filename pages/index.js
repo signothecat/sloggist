f76fb69b9fc19d4aadb0422b2ea3e52470cf4d0f
signothecat <shinogoroneko0818@gmail.com>
2025-10-23 15:35:58 +0900

    fix: set cookie when token invalid on visit
    存在しない・不正なcookieでアクセスした際に
    tokenをCookieに正しくセットするように修正しました。
    
    （これまでの実装では、
    Cookieにuser_tokenが入っていない場合のみCookieをセットしており、
    不正なuser_tokenが入っている場合は無限リダイレクトが発生していました）

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,14 @@ export async function getServerSideProps({ req, res }) {
 
   // Cookieにtokenなし / token: 非文字列 / token: "undefined" などの場合、nullにする
   const safeToken = t => (typeof t === "string" && t !== "undefined" && t !== "null" && t !== "" ? t : null);
-
   const cookieToken = safeToken(getTokenCookie(req));
 
-  const { user, homeSlug } = await bootstrapUser({ token: cookieToken }); // ユーザー作成 & Home作成
-  if (!cookieToken) setTokenCookie(res, user.token); // Cookieにtokenをセット
+  const { user, homeSlug, created } = await bootstrapUser({ token: cookieToken }); // ユーザー作成 & Home作成
+
+  if (!cookieToken || created?.user === true || user.token !== cookieToken) {
+    // 1) Cookieにtokenがない場合、2) ユーザーが新規作成された場合、3) CookieのtokenがDBのuser.tokenと異なる場合(2に進むはずだが念のため)
+    setTokenCookie(res, user.token); // Cookieにtokenをセット
+  }
 
   return {
     redirect: { destination: `/channel/${homeSlug}`, permanent: false },
